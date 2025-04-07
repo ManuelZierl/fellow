f5abb1f049ddeddaece90df80dfaa40be96cdaea
Manuel Zierl <manuel.zierl@web.de>
2025-04-07 10:36:40 +0200

    - add readme - improve lo_message

--- fellow/utils/log_message.py
+++ fellow/utils/log_message.py
@@ -1,10 +1,20 @@
-from fellow.utils.format_message import format_ai_message
+from typing import Optional
+
+from fellow.utils.format_message import format_message
 
 
-def log_message(config, name, color, content, formatter=format_ai_message):
+def log_message(config, name, color, content, language: Optional[str] = None):
     if config.get("log"):
         with open(config["log"], "a", encoding="utf-8") as f:
-            f.write(formatter(name=name, color=color, content=content))
+            f.write(
+                format_message(
+                    name=name,
+                    color=color,
+                    content=content,
+                    language=language,
+                )
+            )
+
 
 def clear_log(config):
     if config.get("log"):
